3d542c4e66b3e78c9736eeedafc163becae8ff64
Pedro <pedroep@live.com>
2022-02-18 20:57:47 -0300

    Ajuste test desafio 3

--- desafio-capgemini-pedro/src/test/java/Desafio3Test.java
+++ desafio-capgemini-pedro/src/test/java/Desafio3Test.java
@@ -13,8 +13,8 @@ public class Desafio3Test {
     }
 
     @Test
-    @DisplayName("Verifica anagrama Roma")
-    public void verificaAnagramaRoma() {
+    @DisplayName("Verifica anagrama ifailuhkqq")
+    public void verificaAnagrama() {
         Anagrama a = new Anagrama();
         String palavra = "ifailuhkqq";
         int i = a.numAnagramas(palavra);
